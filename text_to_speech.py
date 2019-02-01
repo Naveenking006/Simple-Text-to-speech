6b8f4ed05a480228079cd2abb50f9de8aa34bf7c
Naveen kumar <35663494+Naveenking006@users.noreply.github.com>
2019-02-01 23:53:11 +0530

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,11 @@
+
+# importing the pyttsx library 
+import pyttsx3 
+  
+# initialisation 
+engine = pyttsx3.init() 
+  
+# testing 
+engine.say("Hello Dhana , How Are You") 
+engine.say("Thank You") 
+engine.runAndWait() 
